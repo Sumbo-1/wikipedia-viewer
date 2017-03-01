a5f74ef510699146f0d0bcf614707d5809fbab59
Dina <themalni@live.co.uk>
2017-03-01 14:14:14 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -65,7 +65,7 @@ function getArticles(searchValue){
         var url = "https://en.wikipedia.org/wiki/" + title.replace(/\s/g, "_");
 
         // Truncate article title
-        var maxTitleLength = 30;
+        var maxTitleLength = 26;
         var shortTitle = truncateTitle(title, maxTitleLength);
 
         // Display articles on the page
